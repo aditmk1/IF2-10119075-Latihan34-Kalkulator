80c02c28d34cd37ca3b15858cbb2f9f6d1135898
aditmk1 <73748815+aditmk1@users.noreply.github.com>
2020-11-07 21:09:29 +0700

    Add files via upload

---
+++ Kalkulator.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119075.latihan34;
+
+/**
+ *
+ * @author
+ * NAMA     : aditiya musthafa kamil
+ * KELAS    : IF-2
+ * NIM      : 10119075
+ * Deskripsi Program : Class untuk objek Kalkulator
+ * 
+ * 
+ */
+public class Kalkulator {
+    public double value1, value2;
+    
+    public double tambahBilangan() {return (value1+value2);}
+    
+    public double kurangBilangan() {return (value1-value2);}
+    
+    public double kaliBilangan() {return (value1*value2);}
+    
+    public double bagiBilangan() {return (value1/value2);}
+    
+    public double sisaBilangan() {return (value1%value2);}
+}
